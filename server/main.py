08aea3bbbcc96670b4b983e297ae3b38514bf565
Surikkat <Surikkatik@gmail.com>
2023-09-14 16:03:40 +0300

    отрисовка html

--- server/main.py
+++ server/main.py
@@ -1,6 +1,9 @@
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import html, text
 import psycopg2
+import folium
+from folium import plugins
+import branca
 
 app = Sanic("app")
 
@@ -12,7 +15,23 @@ async def test(request, vin: str):
             with conn.cursor() as db_curs:
                 db_curs.execute(f" SELECT lon, lat, \"gpsSpeed\" FROM public.vehicle_data WHERE vin = '{vin}'; ")
                 result = db_curs.fetchall()
-                return text(result)
+
+                world_map = folium.Map(location=[43.23, 76.9], zoom_start=13, prefer_canvas=True, disable_3d=False)
+
+                colormap = branca.colormap.LinearColormap(colors=['blue', 'green', 'yellow', 'red'],
+                                                          index=[10, 40, 60, 90], vmin=0,
+                                                          vmax=110)
+                colormap.caption = 'Vehicle speed'
+                colormap.add_to(world_map)
+
+                heatdata = []
+                for el in result:
+                    heatdata.append([el[1], el[0], el[2]])
+
+                plugins.HeatMap(heatdata, radius=18).add_to(world_map)
+
+                return html(world_map)
+
 
     except Exception as inst:
         print(inst)
